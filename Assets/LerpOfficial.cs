f43a263fe6a85f42bc3df4a7ac9fcecc20839127
Aleks Berg-Jones <bergjones@users.noreply.github.com>
2018-12-16 12:19:36 -0500

    lerp 0

--- Assets/LerpOfficial.cs
+++ Assets/LerpOfficial.cs
@@ -4,28 +4,46 @@ using UnityEngine;
 
 public class LerpOfficial : MonoBehaviour
 {
-    public float lerpSpeed = 2.0f;
     public Vector3 startVec;
     public Vector3 endVec;
+    private float currentLerpTime;
+    public float lerpSpeed = 2.0f;
+    private float timeStartedLerping;
+    private bool shouldLerp = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        endVec = transform.position;
+        //endVec = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyUp("space"))
+        {
+            StartLerping();
+        }
 
-
-        transform.position = Vector3.Lerp(startVec, endVec, Time.deltaTime * lerpSpeed);
-
+        if (shouldLerp)
+        {
+            currentLerpTime += Time.deltaTime;
+            transform.position = Vector3.Lerp(startVec, endVec, currentLerpTime * lerpSpeed);
+        }
     }
 
-    //public Vector3 Lerp(Vector3 start, Vector3 end)
-    //public Vector3 Lerp()
+    private void StartLerping()
+    {
+        currentLerpTime = 0.0f;
+        timeStartedLerping = Time.time;
+        shouldLerp = true;
+    }
+
+    //public Vector3 Lerp(Vector3 start, Vector3 end, float currentLerpTime)
     //{
-        //return Vector3.Lerp(startVec, endVec, Time.deltaTime * lerpSpeed);
+    //    float percent01 = currentLerpTime / lerpSpeed;
+    //    var result = Vector3.Lerp(start, end, percent01);
+
+    //    return result;
     //}
 }
